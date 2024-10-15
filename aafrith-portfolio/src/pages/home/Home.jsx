29efb4903b889f592ab982dfacd4cb2d8e4d38a0
Aafrith <139564438+Aafrith@users.noreply.github.com>
2024-10-15 18:31:23 +0530

    Home page has done

--- aafrith-portfolio/src/pages/home/Home.jsx
+++ aafrith-portfolio/src/pages/home/Home.jsx
@@ -1,8 +1,37 @@
+import Profile from "../../assets/home.jpeg";
+import { Link } from 'react-router-dom';
+import { FaArrowRight } from "react-icons/fa";
+import "./home.css"
 
 const Home = () => {
   return (
-    <section className="home section grid"></section>
+    <section className="home section grid">
+      <img src={Profile} alt="" className="home__img" />
+
+      <div className="home__content">
+        <div className="home__data">
+          <h1 className="home__title">
+            <span>I&apos;m Aafrith. </span>
+            <h2></h2>Software Engineer
+          </h1>
+
+          <p className="home__description">
+            👋 Hi, I&apos;m a Software Engineer with a passion for building innovative softwares. I specialize in software development and have a strong interest in AI and ML.
+          </p>
+
+          <Link to='/about' className='button'>
+            More About Me 
+            <span className='button__icon'>
+              <FaArrowRight />
+            </span>
+          </Link>
+        </div>
+      </div>
+
+      <div className="color__block"></div>
+    </section>
+    
   )
 }
 
-export default Home
+export default Home;
