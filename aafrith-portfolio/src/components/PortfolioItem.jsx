acfdddebfd427f13284294a9da9a25222e94a73f
Aafrith <139564438+Aafrith@users.noreply.github.com>
2024-10-17 11:47:51 +0530

    Close button changed and done for working process

--- aafrith-portfolio/src/components/PortfolioItem.jsx
+++ aafrith-portfolio/src/components/PortfolioItem.jsx
@@ -22,7 +22,19 @@ const PortfolioItem = ({ img, title, details }) => {
         <div className="portfolio__modal">
           <div className="portfolio__modal-content">
             {/* Close button */}
-            <img src={Close} alt="Close" className="modal__close" onClick={toggleModal} />
+            <svg
+                src={Close}
+                className="modal__close"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                onClick={toggleModal} // Add this line
+            >
+                <line x1="18" y1="6" x2="6" y2="18" />
+                <line x1="6" y1="6" x2="18" y2="18" />
+            </svg>
 
             {/* Modal title */}
             <h3 className="modal__title">{title}</h3>
